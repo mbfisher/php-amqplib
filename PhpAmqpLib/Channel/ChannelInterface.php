9309f30e32e5ac59bc6cfd4a1113b05b199ce635
Mike Fisher <michael.fisher@bskyb.com>
2014-11-07 07:52:47 +0000

    Add basic_consume to ChannelInterface

--- PhpAmqpLib/Channel/ChannelInterface.php
+++ PhpAmqpLib/Channel/ChannelInterface.php
@@ -19,4 +19,29 @@ interface ChannelInterface
      * @return mixed
      */
     public function wait($allowed_methods = null, $non_blocking = false, $timeout = 0);
+
+    /**
+     * start a queue consumer
+     * @param string $queue
+     * @param string $consumer_tag
+     * @param bool $no_local
+     * @param bool $no_ack
+     * @param bool $exclusive
+     * @param bool $nowait
+     * @param null $callback
+     * @param null $ticket
+     * @param array $arguments
+     * @return mixed
+     */
+    public function basic_consume(
+        $queue = "",
+        $consumer_tag = "",
+        $no_local = false,
+        $no_ack = false,
+        $exclusive = false,
+        $nowait = false,
+        $callback = null,
+        $ticket = null,
+        $arguments = array()
+    );
 }
